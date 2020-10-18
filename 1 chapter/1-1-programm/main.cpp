98545c1920c27102ad226505b4f262d781ed1904
EugenyX_X <38291801+PistrunXvX@users.noreply.github.com>
2020-10-19 02:59:16 +0300

    Add first programm

---
+++ 1 chapter/1-1-programm/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b, s, p;
+    cout<<"a=";
+    cin>>a;
+    cout<<"b=";
+    cin>>b;
+    p = 2 * (a + b);
+    s = a * b;
+    cout << "Perimetr rectangel has " << p <<endl;
+    cout << "Area rectangel has " << s <<endl;
+    return 0;
+}
